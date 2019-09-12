b23552192c30fd072adfb250a8bbbbad8660003a
clooooode <jackey8616@gmail.com>
2019-09-12 13:57:18 +0800

    Fix polling method in Interface
    Index may not follow order, thus, its need to process the index in each query
     result.

--- tests/tests_1_unit/device/entity/inet/test_u1_interface.py
+++ tests/tests_1_unit/device/entity/inet/test_u1_interface.py
@@ -3,11 +3,11 @@ from yunnms.device.entity import Interface
 
 def test_str():
     i = Interface(name="test_interface", int_type="ethernetCsmacd",
-                  mtu=1000, speed=1000)
-    assert str(i) == "Interface<NAME={}, PH_ADDR={}>".format(i.name, i.phisical_address)
+                  mtu=1000, speed=1000, status="up")
+    assert str(i) == "Interface<NAME={}, STA: {}, PH_ADDR={}>".format(i.name, i.status, i.phisical_address)
 
 
 def test_repr():
     i = Interface(name="test_interface", int_type="ethernetCsmacd",
-                  mtu=1000, speed=1000)
+                  mtu=1000, speed=1000, status="up")
     assert repr(i) == str(i)
--- yunnms/device/entity/inet/interface.py
+++ yunnms/device/entity/inet/interface.py
@@ -7,20 +7,33 @@ from yunnms.device.abc import SNMPv3PollingAbc
 class Interface(SNMPv3PollingAbc):
     @staticmethod
     def polling(snmp_conn: "SNMPv3Connection") -> "Interface":
-        return [
-            Interface(*each.values())
-            for each in snmp_conn.bulk_by(
-                oids=[
-                    ("IF-MIB", "ifName"),
-                    ("IF-MIB", "ifDescr"),
-                    ("IF-MIB", "ifType"),
-                    ("IF-MIB", "ifMtu"),
-                    ("IF-MIB", "ifSpeed"),
-                    ("IF-MIB", "ifPhysAddress"),
-                ],
-                count_oid=("IF-MIB", "ifNumber", 0),
+        interfaces = []
+        prekey = "IF-MIB::"
+        for each in snmp_conn.bulk_by(
+            oids=[
+                ("IF-MIB", "ifName"),
+                ("IF-MIB", "ifDescr"),
+                ("IF-MIB", "ifType"),
+                ("IF-MIB", "ifMtu"),
+                ("IF-MIB", "ifSpeed"),
+                ("IF-MIB", "ifPhysAddress"),
+                ("IF-MIB", "ifAdminStatus"),
+            ],
+            count_oid=("IF-MIB", "ifNumber", 0),
+        ):
+            index = list(each.keys())[0].split(".")[1]
+            interfaces.append(
+                Interface(
+                    name=each[prekey + "ifName." + index],
+                    int_type=each[prekey + "ifType." + index],
+                    mtu=int(each[prekey + "ifMtu." + index]),
+                    speed=int(each[prekey + "ifSpeed." + index]),
+                    status=each[prekey + "ifAdminStatus." + index],
+                    description=each[prekey + "ifDescr." + index],
+                    phisical_address=each[prekey + "ifPhysAddress." + index],
+                )
             )
-        ]
+        return interfaces
 
     def __init__(
         self,
@@ -28,6 +41,7 @@ class Interface(SNMPv3PollingAbc):
         int_type: str,
         mtu: int,
         speed: int,
+        status: str,
         description: str = "",
         phisical_address: str = "",
     ) -> None:
@@ -36,10 +50,11 @@ class Interface(SNMPv3PollingAbc):
         self.int_type = int_type
         self.mtu = mtu
         self.speed = speed
+        self.status = status
         self.phisical_address = phisical_address
 
     def __str__(self) -> str:
-        return "Interface<NAME={}, PH_ADDR={}>".format(self.name, self.phisical_address)
+        return "Interface<NAME={}, STA: {}, PH_ADDR={}>".format(self.name, self.status, self.phisical_address)
 
     def __repr__(self) -> str:
         return self.__str__()
